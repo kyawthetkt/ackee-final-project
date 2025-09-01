2cd246d9f5cb95e46e58cc3c83546c84c119ec83
kyawthet <kyawthet.2017.flc@gmail.com>
2025-09-02 01:25:32 +0630

    Fix

--- src/app/textfeed/create/page.jsx
+++ src/app/textfeed/create/page.jsx
@@ -1,4 +1,11 @@
-import TextfeedCreate from '@/components/textfeed/textfeed-create-feature'
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const TextfeedCreate = dynamic(
+  () => import('@/components/textfeed/textfeed-create-feature'),
+  { ssr: false }
+);
 
 export default function Page() {
   return <TextfeedCreate />
