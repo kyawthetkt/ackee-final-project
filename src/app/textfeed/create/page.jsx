6ab186a0cb72babb170d00759197b31f5a10d0db
kyawthet <kyawthet.2017.flc@gmail.com>
2025-09-02 00:56:35 +0630

    Fix

--- src/app/textfeed/create/page.jsx
+++ src/app/textfeed/create/page.jsx
@@ -1,5 +1,8 @@
 import TextfeedCreate from '@/components/textfeed/textfeed-create-feature'
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export default function Page() {
   return <TextfeedCreate />
 }
--- src/app/textfeed/page.tsx
+++ src/app/textfeed/page.tsx
@@ -1,5 +1,8 @@
 import TextFeedListFeature from '@/components/textfeed/textfeed-list-feature'
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export default function Page() {
   return <TextFeedListFeature />
 }
